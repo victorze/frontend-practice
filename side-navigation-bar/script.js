1d334cc768f59b6e365200f65c3cd03e03229cb1
Victor <victorze@protonmail.com>
2021-08-27 19:43:56 -0500

    Add Side Navigation Bar project

---
+++ side-navigation-bar/script.js
@@ -0,0 +1,15 @@
+const arrow = document.querySelectorAll(".arrow");
+
+for (let i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e) => {
+    let arrowParent = e.target.parentElement.parentElement;
+    arrowParent.classList.toggle("showMenu");
+  })
+}
+
+const sidebar =  document.querySelector(".sidebar");
+const sidebarBtn =  document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
